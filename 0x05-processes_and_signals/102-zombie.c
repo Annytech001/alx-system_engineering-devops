002074b56998727f61fb06ce77dc1037ecf20dfb
Annytech001 <godlybassey101@gmail.com>
2023-10-02 15:41:10 -0700

    loops

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,28 @@
+
+/**
+ * main - Creates five zombie processes.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t pid;
+	char count = 0;
+
+	while (count < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++;
+		}
+		else
+			exit(0);
+	}
+
+	infinite_while();
+
+	return (EXIT_SUCCESS);
+
